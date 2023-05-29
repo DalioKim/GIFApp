5752493e22055bb1cf476c740fa2a2898b56cb05
kdh <ejxlabvlzhxm@naver.com>
2023-05-29 22:10:11 +0900

    feat: Domain/User

---
+++ GIFApp/Domain/User/GeneralUser.swift
@@ -0,0 +1,13 @@
+//
+//  GeneralUser.swift
+//  GIFApp
+//
+//  Created by 김동현 on 2023/05/29.
+//
+
+import Foundation
+
+struct GeneralUser: User {
+    var nickName: String?
+    var email: String?
+}
---
+++ GIFApp/Domain/User/User.swift
@@ -0,0 +1,17 @@
+//
+//  Member.swift
+//  GIFApp
+//
+//  Created by 김동현 on 2023/05/29.
+//
+
+import Foundation
+
+protocol User {
+    var nickName: String? {
+        get set
+    }
+    var email: String? {
+        get set
+    }
+}
